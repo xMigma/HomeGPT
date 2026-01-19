34ffd4ca319b67c7c2512eae865adc1fed21f3eb
xMigma <miguelmh04@hotmail.com>
2026-01-19 11:54:16 +0100

    Remove web search functionality and related providers

--- voice-assistant/web/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import WebSearchProvider, make_web_search, fetch_full_text
-from .brave import BraveProvider
-
-__all__ = [
-    "WebSearchProvider",
-    "make_web_search",
-    "fetch_full_text",
-    "BraveProvider",
-]
